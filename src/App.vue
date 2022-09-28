57c4f73571db646bbc316b81bcc7992e255c0f2a
bbangsol <rlaekthf1114@naver.com>
2022-09-28 11:05:53 +0900

    scss 적용

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+</style>
--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>abbout</h1>
+  <h1>about</h1>
 </template>
